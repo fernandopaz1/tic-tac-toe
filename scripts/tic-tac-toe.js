deca4b77871b38892f828d1b20fbab5c718d0a9a
Fernando Paz <ferpaz9292@gmail.com>
2020-09-23 21:56:45 -0300

    agrego eventListener a los cuadrados

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,21 +1,28 @@
-function addSquare() {
-  return '<div class="square">&nbsp</div>';
-}
+const addBoard = (files, rows) => {
+	const addSquare = () => '<div class="square">&nbsp</div>';
 
-function addSquareFile(cantColumnas) {
-  let file = "";
-  for (let i = 0; i < cantColumnas; i++) {
-    file += addSquare();
-  }
-  document.querySelector(
-    ".boardContainer"
-  ).innerHTML += `<div class="file">${file}</div>`;
-}
+	function addSquareFile(cantColumnas) {
+		let file = "";
+		for (let i = 0; i < cantColumnas; i++) {
+			file += addSquare();
+		}
+		document.querySelector(
+			".boardContainer"
+		).innerHTML += `<div class="file">${file}</div>`;
+	}
 
-function addBoard(files, rows) {
-  for (let i = 0; i < files; i++) {
-    addSquareFile(rows);
-  }
-}
+	for (let i = 0; i < files; i++) {
+		addSquareFile(rows);
+	}
+};
 
 addBoard(3, 3);
+
+document.querySelectorAll("div.square").forEach(square =>{
+	square.addEventListener("click",(e)=>{
+		let element= e.target;
+		element.classList.add("completed");
+		element.innerHTML="X";
+	})
+})
+
