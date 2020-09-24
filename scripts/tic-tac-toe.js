5f26d841150c7b4e0c66a6df3c252c8231211805
Fernando Paz <ferpaz9292@gmail.com>
2020-09-24 01:22:11 -0300

    comento factorySolucion (probablemente sea eliminado) y mejoro css de div.square

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -39,13 +39,87 @@ const game = Game();
 const squares = document.querySelectorAll("div.square");
 
 
+// const Solucion = () => {
+// 	// let primeraFila = 0;
+// 	// let segundaFila = 0
+// 	// let tercerFila = 0;
+// 	let soluciones=new Array(12).fill(0);
+// 	this.sumarSolucion = (i,j) => {
+// 		let index=i+3*j;
+// 		switch(index){
+// 			case 0:
+// 				soluciones[0]++;
+// 				soluciones[3]++;
+// 				soluciones[6]++;
+// 				soluciones[9]++;
+// 				soluciones[11]++;
+// 				return;
+// 			case 1:
+// 				soluciones[0]++;
+// 				soluciones[4]++;
+// 				soluciones[10]++;
+// 				return;
+// 			case 2:
+// 				soluciones[0]++;
+// 				soluciones[5]++;
+// 				soluciones[7]++;
+// 				soluciones[8]++;
+// 				return;
+// 			case 3:
+// 				soluciones[1]++;
+// 				soluciones[3]++;
+// 				soluciones[8]++;
+// 				return;
+// 			case 4:
+// 				soluciones[1]++;
+// 				soluciones[4]++;
+// 				soluciones[6]++;
+// 				soluciones[7]++;
+// 				return;
+// 			case 5:
+// 				soluciones[1]++;
+// 				soluciones[5]++;
+// 				soluciones[9]++;
+// 				soluciones[10]++;
+// 				soluciones[11]++;
+// 				return;
+// 			case 6:
+// 				soluciones[2]++;
+// 				soluciones[3]++;
+// 				soluciones[7]++;
+// 				soluciones[10]++;
+// 				return;
+// 			case 7:
+// 				soluciones[2]++;
+// 				soluciones[4]++;
+// 				soluciones[8]++;
+// 				soluciones[9]++;
+// 				soluciones[11]++;
+// 				return;
+// 			case 8:
+// 				soluciones[2]++;
+// 				soluciones[5]++;
+// 				soluciones[6]++;
+// 		}
+// 	}
+// 	return {soluciones,sumarSolucion}
+// }
+
 
 
 const Gameboard = () => {
 	let tableroArray = [];
-	squares.forEach(square =>tableroArray.push(square));
-	let board= [tableroArray.slice(0,3),tableroArray.slice(3,6),tableroArray.slice(6,9)];
-	return {board}	
+	squares.forEach(square => tableroArray.push(square));
+	let board = [tableroArray.slice(0, 3), tableroArray.slice(3, 6), tableroArray.slice(6, 9)];
+	this.contenido = (i,j) => {
+		return board[i][j].textContent;
+	}
+	this.estaOcupado = (i, j) => {
+		return contenido(i,j) != "" ? true : false;
+	}
+
+	
+	return { board, contenido, estaOcupado }
 }
 
 
