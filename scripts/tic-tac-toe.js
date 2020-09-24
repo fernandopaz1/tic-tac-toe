d2f804797c8b1208c796eeccd613fba9b0283626
Fernando Paz <ferpaz9292@gmail.com>
2020-09-24 02:53:21 -0300

    La logica del juego funciona es un Tic-Tac-Toc toroidal

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,4 +1,4 @@
-const Board = (files, rows) => {
+const inicializarCeldas = (files, rows) => {
 	const addSquare = () => '<div class="square"></div>';
 
 	function addSquareFile(cantColumnas) {
@@ -16,22 +16,86 @@ const Board = (files, rows) => {
 	}
 };
 
-Board(3, 3);
 
 const Player = (name, piece) => {
 	this.getName = () => name;
 	this.getPiece = () => piece;
 	return { name, piece, getName, getPiece };
 }
+inicializarCeldas(3, 3);
+
+const Gameboard = () => {
+	let tableroArray = [];
+	let squares = document.querySelectorAll("div.square");
+	squares.forEach(square => tableroArray.push(square));
+	let board = [tableroArray.slice(0, 3), tableroArray.slice(3, 6), tableroArray.slice(6, 9)];
+	this.contenido = (i, j) => {
+		return board[i][j].textContent;
+	}
+
+
+	function ganoEnLaFila(i, pieza) {
+		let result = true;
+		for (let j = 0; j < 3; j++) {
+			result = result && contenido(i, j) == pieza;
+		}
+		return result;
+	}
+	function ganoEnLaColumna(i, pieza) {
+		let result = true;
+		for (let j = 0; j < 3; j++) {
+			result = result && contenido(j, i) == pieza;
+		}
+		return result;
+	}
+	function ganoEnLaDiagonalDerecha(pieza, despl) {
+		let result = true;
+		for (let j = 0; j < 3; j++) {
+			result = result && contenido((j + despl) % 3, j) == pieza;
+
+		}
+		return result;
+	}
+
+	function ganoEnLaDiagonalIzquierda(pieza, despl) {
+		let result = true;
+		for (let j = 0; j < 3; j++) {
+			result = result && contenido((j + despl) % 3, 2 - j) == pieza;
+
+		}
+		return result;
+	}
+
+	const ganador = (pieza) => {
+		let result = false;
+		for (let i = 0; i < 3; i++) {
+			result = result || ganoEnLaColumna(i, pieza);
+			result = result || ganoEnLaFila(i, pieza);
+			result = result || ganoEnLaDiagonalDerecha(pieza, i);
+			result = result || ganoEnLaDiagonalIzquierda(pieza, i);
+		}
+		return result
+	}
+
+
+	return { board, ganador }
+}
+
 
 const Game = () => {
 	let player1 = Player("hola", "X");
 	let player2 = Player("chau", "O");
 	let turn = true;
+	let board = Gameboard();
 	this.changeTurn = () => { turn = !turn };
 	this.actualPlayer = () => { return turn ? player1 : player2 };
+	this.actualName = () => actualPlayer().getName();
 	this.actualPiece = () => actualPlayer().getPiece();
-	return { player1, player2, turn, changeTurn, actualPlayer, actualPiece }
+	this.isWinner = () => board.ganador(actualPiece());
+	this.play = (element) => {
+
+	}
+	return { player1, player2, turn, changeTurn, actualPlayer, actualPiece, actualName, isWinner }
 }
 
 const game = Game();
@@ -39,88 +103,7 @@ const game = Game();
 const squares = document.querySelectorAll("div.square");
 
 
-// const Solucion = () => {
-// 	// let primeraFila = 0;
-// 	// let segundaFila = 0
-// 	// let tercerFila = 0;
-// 	let soluciones=new Array(12).fill(0);
-// 	this.sumarSolucion = (i,j) => {
-// 		let index=i+3*j;
-// 		switch(index){
-// 			case 0:
-// 				soluciones[0]++;
-// 				soluciones[3]++;
-// 				soluciones[6]++;
-// 				soluciones[9]++;
-// 				soluciones[11]++;
-// 				return;
-// 			case 1:
-// 				soluciones[0]++;
-// 				soluciones[4]++;
-// 				soluciones[10]++;
-// 				return;
-// 			case 2:
-// 				soluciones[0]++;
-// 				soluciones[5]++;
-// 				soluciones[7]++;
-// 				soluciones[8]++;
-// 				return;
-// 			case 3:
-// 				soluciones[1]++;
-// 				soluciones[3]++;
-// 				soluciones[8]++;
-// 				return;
-// 			case 4:
-// 				soluciones[1]++;
-// 				soluciones[4]++;
-// 				soluciones[6]++;
-// 				soluciones[7]++;
-// 				return;
-// 			case 5:
-// 				soluciones[1]++;
-// 				soluciones[5]++;
-// 				soluciones[9]++;
-// 				soluciones[10]++;
-// 				soluciones[11]++;
-// 				return;
-// 			case 6:
-// 				soluciones[2]++;
-// 				soluciones[3]++;
-// 				soluciones[7]++;
-// 				soluciones[10]++;
-// 				return;
-// 			case 7:
-// 				soluciones[2]++;
-// 				soluciones[4]++;
-// 				soluciones[8]++;
-// 				soluciones[9]++;
-// 				soluciones[11]++;
-// 				return;
-// 			case 8:
-// 				soluciones[2]++;
-// 				soluciones[5]++;
-// 				soluciones[6]++;
-// 		}
-// 	}
-// 	return {soluciones,sumarSolucion}
-// }
 
-
-
-const Gameboard = () => {
-	let tableroArray = [];
-	squares.forEach(square => tableroArray.push(square));
-	let board = [tableroArray.slice(0, 3), tableroArray.slice(3, 6), tableroArray.slice(6, 9)];
-	this.contenido = (i,j) => {
-		return board[i][j].textContent;
-	}
-	this.estaOcupado = (i, j) => {
-		return contenido(i,j) != "" ? true : false;
-	}
-
-	
-	return { board, contenido, estaOcupado }
-}
 
 
 squares.forEach(square =>
@@ -129,9 +112,17 @@ squares.forEach(square =>
 		if (element.innerHTML == "") {
 			element.classList.add("completed");
 			element.innerHTML = game.actualPiece();
+			if (game.isWinner()) {
+				endGame(game.actualName())
+				return;
+			}
 			game.changeTurn();
 		}
 	})
 )
 
+const endGame = (winnerName) => {
+	let message = document.querySelector("#winnerMessage");
+	message.innerText = `The winner is ${winnerName}`
+}
 
